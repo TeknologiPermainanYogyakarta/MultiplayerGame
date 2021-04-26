7fe23da7392642e6a1c7bb0dcee916c673c0b2c8
hassanshin <f3fahmi@gmail.com>
2021-04-26 15:41:42 +0700

    -2 type of combo

--- Assets/Scripts/Combo Attack/ComboAttack.cs
+++ Assets/Scripts/Combo Attack/ComboAttack.cs
@@ -8,7 +8,7 @@ public class ComboAttack : MonoBehaviour
 {
     [SerializeField]
     private float leeway = 1f;
-    public Combo comboAttack;
+    public ComboData comboMoves;
 
     [Header("Components")]
     [SerializeField] private int nextAttackIndex;
@@ -18,102 +18,124 @@ public class ComboAttack : MonoBehaviour
 
     [SerializeField] private bool ignoreInput;
     [SerializeField] private bool isAnimating;
+    [SerializeField] public bool isCombo;
 
     [Header("Debug")]
     [SerializeField] private UnityEngine.UI.Image waitComboBar;
 
+    private void Start()
+    {
+        comboMoves.UpdatePossibleAttack(nextAttackIndex);
+    }
+
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !ignoreInput)
         {
-            if (!ignoreInput)
-            {
-                animateAttack();
-            }
+            processInput(AttackType.heavy);
+        }
+        else if (Input.GetButtonDown("Fire2") && !ignoreInput)
+        {
+            processInput(AttackType.light);
         }
     }
 
-    private void animateAttack()
+    private void processInput(AttackType input)
     {
+        Attack att = comboMoves.GetAttackData(input);
+
+        if (att == null)
+        {
+            ResetCombo();
+
+            att = comboMoves.GetAttackData(input);
+        }
+
+        animateAttack(att);
+    }
+
+    private void animateAttack(Attack att)
+    {
+        if (isAnimating)
+        {
+            isCombo = true;
+        }
+
         isAnimating = true;
         ignoreInput = true;
 
-        if (nextAttackIndex >= comboAttack.Attack.Count)
-        {
-            nextAttackIndex = 0;
-        }
-
-        Attack att = comboAttack.Attack[nextAttackIndex];
         anim.Play(att.Name, -1, 0);
     }
 
-    // referenced in animation event
-    public void DamageTarget()
+    public void DamageTarget() // Referenced in animation event
     {
         ignoreInput = false;
         nextAttackIndex++;
+        comboMoves.UpdatePossibleAttack(nextAttackIndex);
+
+        // Damage enemy in front
+    }
+
+    public void Idling()
+    {
+        isCombo = false;
+        ResetCombo();
     }
 
     public void DoneAttackAnim()
     {
         isAnimating = false;
-
-        forceStopWaitCombo();
-        startWaitNewCombo();
-
-        if (nextAttackIndex >= comboAttack.Attack.Count) // if combo finished in attack anim
-        {
-            resetCombo();
-            forceStopWaitCombo();
-        }
     }
 
-    private void resetCombo()
+    public void ResetCombo()
     {
         nextAttackIndex = 0;
+        comboMoves.UpdatePossibleAttack(nextAttackIndex);
     }
-
-    #region wait combo leeway
-
-    private void startWaitNewCombo()
-    {
-        LeanTween.value(gameObject, updateWaitCombo, 0, 1, leeway).setOnComplete(completeWaitCombo);
-        waitComboBar.gameObject.SetActive(true);
-    }
-
-    private void updateWaitCombo(float hi)
-    {
-    }
-
-    private void completeWaitCombo()
-    {
-        waitComboBar.gameObject.SetActive(false);
-
-        resetCombo();
-    }
-
-    private void forceStopWaitCombo()
-    {
-        if (LeanTween.isTweening(this.gameObject))
-        {
-            LeanTween.cancel(this.gameObject);
-        }
-    }
-
-    #endregion wait combo leeway
 }
 
 [System.Serializable]
 public class Attack
 {
     public string Name;
-    public AttackType Inputs;
+    public AttackType InputType;
 }
 
 [System.Serializable]
-public class Combo
+public class ComboMove
 {
-    public List<Attack> Attack;
+    public string AttackName;
+    public List<Attack> Attack = new List<Attack>();
+}
+
+[System.Serializable]
+public class ComboData
+{
+    public List<ComboMove> ComboMoves = new List<ComboMove>();
+    public List<Attack> PossibleAttack = new List<Attack>();
+
+    public Attack GetAttackData(AttackType input)
+    {
+        for (int i = 0; i < PossibleAttack.Count; i++)
+        {
+            if (PossibleAttack[i].InputType == input)
+            {
+                return PossibleAttack[i];
+            }
+        }
+
+        return null;
+    }
+
+    public void UpdatePossibleAttack(int index)
+    {
+        PossibleAttack.Clear();
+        foreach (var move in ComboMoves)
+        {
+            if (move.Attack.Count > index)
+                PossibleAttack.Add(move.Attack[index]);
+        }
+    }
 }
 
 public enum AttackType { heavy, light };
--- Assets/Scripts/Combo Attack/finishedAnim.cs
+++ Assets/Scripts/Combo Attack/finishedAnim.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class finishedAnim : StateMachineBehaviour
 {
+    public bool isIdle;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (isIdle)
+        {
+            animator.GetComponent<ComboAttack>().Idling();
+        }
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
